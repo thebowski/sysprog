e810df3edcdce9e3bd91bd614668a73fcbac1434
Alex <aeb2814@rit.edu>
2016-05-19 17:24:02 -0400

    Added comments on headers

--- src/netconv.h
+++ src/netconv.h
@@ -3,9 +3,44 @@
 
 #include "types.h"
 
+/*
+** Converts byte order for network transmission for 32 bit values
+**
+** Arguments:
+**     uint32_t hostlong - A 32-bit integer in host byte order
+** Returns:
+**     uint43_t ---------- A 32-bit integer in network byte order
+*/
 uint32_t htonl(uint32_t hostlong);
+
+/*
+** Converts byte order for network transmission for 16 bit values
+**
+** Arguments:
+**     uint16_t hostshort - A 16-bit integer in host byte order
+** Returns:
+**     uint16_t ----------- A 16-bit integer in network byte order
+*/
 uint16_t htons(uint16_t hostshort);
+
+/*
+** Converts byte order for host parsing for 32 bit values
+**
+** Arguments:
+**     uint32_t hostlong - A 32-bit integer in network byte order
+** Returns:
+**     uint32_t ---------- A 32-bit integer in host byte order
+*/
 uint32_t ntohl(uint32_t netlong);
+
+/*
+** Converts byte order for host parsing for 16 bit values
+**
+** Arguments:
+**     uint16_t hostlong - A 16-bit integer in network byte order
+** Returns:
+**     uint16_t ---------- A 16-bit integer in hostx byte order
+*/
 uint16_t ntohs(uint16_t netshort);
 
 #endif
--- src/netdrv.h
+++ src/netdrv.h
@@ -5,8 +5,23 @@
 #include "network.h"
 #include "ip.h"
 
+/*
+** _dev_send
+**
+** Pretends to send the packets.  Actually just sends the packets back to the
+** parsing function.
+**
+** Aguments:
+**     packet_t buf[] - A buffer of packets to send
+**     uint16_t size -- The number of packets in the buffer
+*/
 void _dev_send( packet_t buf[], uint16_t size );
 
+/*
+** _dev_receive
+**
+** A stub for the driver receive function.  Does nothing at all
+*/
 void _dev_recieve( void );
 
 #endif
