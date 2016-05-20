0e7143f31069af2c370f129a0271db88723d32b9
Alexander Bobowski <aeb2814@rit.edu>
2016-05-20 09:35:02 -0400

    Removed unnecessary debug print statements

--- src/netdrv.c
+++ src/netdrv.c
@@ -1,8 +1,6 @@
 #include "netdrv.h"
-#include "c_io.h"
 #include "syscall.h"
 void _dev_send( packet_t buf[], uint16_t size ){
-        c_printf( "Entered netdrv.c send routine\n" );
         _receive( buf, size );
 }
 
